55f87b1a87fa639fad18fd91ed2969cecd0ba171
Harshit sharma <programming966051@gmail.com>
2024-11-06 20:11:03 +0530

    make data base cloudly

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const app = express();
 const axios = require('axios')
 const path = require('path')
 const cors = require('cors');
-require('./connection/connections')
-
+const connectDB = require('./connection/connections')
+require('dotenv').config()
+connectDB()
+  .then(() => {
+    console.log('Database connected successfully');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Database connection failed:', error);
+  });
 
 
 
@@ -26,9 +36,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/purchase',PurchaseRoute)
 
-app.listen(3000 ,()=>{
-    console.log('app listen at port number 3000')
-})
 app.get('/',(req,res)=>{
     res.send('api work correctly')
 })
--- connection/connections.js
+++ connection/connections.js
@@ -1,10 +1,24 @@
 
-const mongoose = require("mongoose");
-main().catch(err => console.log(err));
+// const mongoose = require("mongoose");
+// main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/Jkcompany');
-console.log("mongoose responsed sucessfully");
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:27017/Jkcompany');
+// console.log("mongoose responsed sucessfully");
 
-}
+// }
+const mongoose = require('mongoose');
+require('dotenv').config
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODBATLS, {
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+ module.exports = connectDB;
 
