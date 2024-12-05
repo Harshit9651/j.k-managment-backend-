9e0f2a79617d5dd18520e442eaee9efeb8613a0a
Harshit sharma <programming966051@gmail.com>
2024-12-05 13:24:57 +0530

    add route

--- routes/adminroute.js
+++ routes/adminroute.js
@@ -2,6 +2,4 @@ const express = require("express");
 const router = express.Router();
 const AdminController = require('../controllers/admincontroller')
 
-router.post('/Login',AuthController.login)
-
 module.exports = router;
