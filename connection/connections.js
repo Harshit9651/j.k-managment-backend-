7c920e8b4b6bd6caa9a861b89df1352306e3fe37
Harshit sharma <programming966051@gmail.com>
2024-12-15 11:44:10 +0530

    add cloud URL

--- connection/connections.js
+++ connection/connections.js
@@ -12,8 +12,11 @@ require('dotenv').config
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODBATLS, {
-    });
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000  });
     console.log('MongoDB connected');
+
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
